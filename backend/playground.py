363220b23350dee2621e9eca5cf6ac48b8c3d57b
MaxymHybalo <keepass.log@gmail.com>
2022-05-07 01:52:26 +0300

    playground

--- backend/playground.py
+++ backend/playground.py
@@ -1,7 +1,7 @@
-from ocr import get_awaking
-from utils import awaking_aggregator, reporter
-from driver import send
-if __name__ == '__main__':
+# from ocr import get_awaking
+# from utils import awaking_aggregator, reporter
+# from driver import send
+# if __name__ == '__main__':
     # res = awaking_aggregator.normalize(get_awaking())
     # comp = awaking_aggregator.compare(res, 'configs/stats_set_1.yml')
     # print(res)
@@ -10,3 +10,15 @@ if __name__ == '__main__':
     # reporter.initialize('awaking', file)
 
     # reporter.report('awaking', file, (0, res, comp, 'test file name'))
+
+
+from PyQt6.QtWidgets import QApplication, QWidget
+
+import sys # Только для доступа к аргументам командной строки
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()  # Важно: окно по умолчанию скрыто.
+
+app.exec()
