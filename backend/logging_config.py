f7679265a99df084e7b642d310a0454b9648cac0
MaxymHybalo <keepass.log@gmail.com>
2024-12-22 17:21:10 +0200

    add colored logs

--- backend/logging_config.py
+++ backend/logging_config.py
@@ -1,10 +1,33 @@
 import logging
+import colorlog
 
 def setup_logging():
-    logging.basicConfig(level=logging.DEBUG,
-                        format='[%(levelname)s] - %(asctime)s | %(name)s | %(message)s',
-                        handlers=[
-                            logging.FileHandler("debug.log"),
-                            logging.StreamHandler()
-                        ])
-    logging.getLogger().setLevel(logging.DEBUG)
+    log_format = (
+        '%(log_color)s[%(levelname)s] - %(asctime)s | %(name)s | %(message)s'
+    )
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(log_color)s[%(levelname)s] - %(asctime)s | %(name)s | %(message)s',
+    #                     handlers=[
+    #                         logging.FileHandler("debug.log"),
+    #                         logging.StreamHandler()
+    #                     ])
+    log_colors = {
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+    }
+    formatter = colorlog.ColoredFormatter(log_format, log_colors=log_colors)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('debug.log')
+    file_handler.setFormatter(logging.Formatter(
+        '[%(levelname)s] - %(asctime)s | %(name)s | %(message)s'
+    ))
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
